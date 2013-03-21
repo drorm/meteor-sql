d360601550bf44ddf009d1256c95dac0bd7f40f5
Dror <dror@devwik.com>
2013-03-21 13:44:21 -0700

    Changed the select example to show aggregates

--- server/tests.js
+++ server/tests.js
@@ -1,6 +1,6 @@
 Devwik.SQL.runTests = function() {
 	//Example of a select with a join
-	var select = new Devwik.SQL.Select('empsCities', 'select employees.*, offices.city from employees, offices where offices.officeCode = employees.officecode');
+	var select = new Devwik.SQL.Select('empsPerCity', 'select count(*) empNumber,  offices.* from employees, offices where offices.officeCode = employees.officecode group by officeCode');
 	//Devwik.SQL.Tests.transactions();
 };
 
