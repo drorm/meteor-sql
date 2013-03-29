68e8a3e62c634cd527de1a89775f89b756e233fc
Dror <dror@devwik.com>
2013-03-28 19:31:37 -0700

    Remove unnecessary logging

--- client/example/select.js
+++ client/example/select.js
@@ -1,9 +1,7 @@
 var empCities = new Meteor.Select('empsPerCity');
-console.log(empCities);
 
 
 	Template.devwikSelects.selects = function () {
-		console.log('selects');
 		return empCities.find({}, {sort: {employeeNumber: -1}});
 	};
 
